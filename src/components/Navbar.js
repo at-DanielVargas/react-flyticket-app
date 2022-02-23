d33471b52019767fc036d3efe58ccf221832b69f
at-DanielVargas <daniel.vargastr@gmail.com>
2022-02-22 21:17:06 -0600

    chore: initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
   return (
     <div className="header">
-      <div className="navbar">
-        <Link to="/" className="logo">
-          <div className="icon">
-            <i class="fa-solid fa-ticket"></i>
-            <i class="fa-solid fa-plane"></i>
-          </div>
-          <div className="text">
-            Fly<span className="txt-red">Ticket</span>
-          </div>
-        </Link>
-
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/flights">Flights</Link>
-          <Link to="/cart">Cart</Link>
-        </nav>
+      <div className="container">
+        <div className="navbar">
+          <Link to="/" className="logo">
+            <div className="icon">
+              <i class="fa-solid fa-ticket"></i>
+              <i class="fa-solid fa-plane"></i>
+            </div>
+            <div className="text">
+              Fly<span className="txt-red">Ticket</span>
+            </div>
+          </Link>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/flights">Flights</Link>
+            <Link to="/cart">Cart</Link>
+          </nav>
+        </div>
       </div>
     </div>
   );
